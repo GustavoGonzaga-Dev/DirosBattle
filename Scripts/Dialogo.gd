c2ba6003287005621e31aee17065ec7ca5855512
GustavoGonzaga-Dev <69990378+GustavoGonzaga-Dev@users.noreply.github.com>
2021-08-04 20:08:53 -0300

    Arrumando o erro de touch
    na tela do escolha seu ovo, tinha o erro por causa do touchButton, arrumei a logica e troquei pra textureButton

--- Scripts/Dialogo.gd
+++ Scripts/Dialogo.gd
@@ -14,6 +14,8 @@ enum State{
 
 var stadoAtual = State.READY
 var textQueue = []
+var bt = 1
+var ovo
 
 func _ready():
 	hide_TextBox()
@@ -27,24 +29,25 @@ func _ready():
 	FtextQueue("Mas antes eu preciso saber de algo...")
 	FtextQueue("O que você Prefere?")
 	FtextQueue("Muito bom, aqui está o seu OVODIRO...")
-	FtextQueue("CUIDE BEM DELE.")
+	FtextQueue("CUIDE MUITO BEM DELE.")
 
 func _process(delta):
 	match stadoAtual:
 		State.READY:
 			if !textQueue.empty():
+				bt = 1
 				Display_text()
 		State.READING:
-			if Input.is_action_just_pressed("ui_accept"):
+			if bt == 2:
 				lblText.percent_visible = 1.0
 				$TextBox/Tween.stop_all()
 				endS.text = ">"
 				changeState(State.FINISHED)
 		State.FINISHED:
-			if Input.is_action_just_pressed("ui_accept"):
+			if bt == 3:
 				changeState(State.READY)
 				hide_TextBox()
-	
+
 func FtextQueue(nextText):
 	textQueue.push_back(nextText)
 
@@ -53,7 +56,7 @@ func hide_TextBox():
 	endS.text = ""
 	lblText.text = ""
 	textboxContainer.hide()
-	
+
 func show_TextBox():
 	startS.text = "*"
 	textboxContainer.show()
@@ -61,8 +64,18 @@ func show_TextBox():
 func Display_text():
 	var nextText = textQueue.pop_front()
 	if nextText == "O que você Prefere?":
+		$TextBox/btn.visible = not $TextBox/btn.visible
+		$TextBox/Proximo.visible = not $TextBox/Proximo.visible
 		$TextBox/TextBoxContainer/AnimationPlayerTxtBox.play("subindo")
-	if nextText == "CUIDE BEM DELE.":
+	if nextText == "Muito bom, aqui está o seu OVODIRO...":
+		$TextBox/Proximo.visible = not $TextBox/Proximo.visible
+		$TextBox/btn.visible = not $TextBox/btn.visible
+	if nextText == "CUIDE MUITO BEM DELE.":
+		$TextBox/Proximo/AnimeProximo.play("Sumindo")
+		if ovo == "A":
+			$"Ovos/OVO-BLUE/AnimationOvoAzul".play("Sumindo")
+		elif ovo == "B":
+			$"Ovos/OVO-ROSA/AnimationOvoRosa".play("Sumindo")
 		$"transição/ColorRect/AnimacaoTransicao".play("Saindo")
 	lblText.text = nextText
 	lblText.percent_visible = 0.0
@@ -73,11 +86,12 @@ func Display_text():
 
 func _on_Tween_tween_all_completed():
 	endS.text = ">"
+	_on_btn_pressed()
 	changeState(State.FINISHED)
 
 func changeState(nextState):
 	stadoAtual = nextState
-	
+
 func _on_AnimacaoTransicao_animation_finished(anim_name):
 	if anim_name == "Entrando":
 		$planeta/planeta1/AnimationPlayerPlaneta.play("RodaRodaJequiti")
@@ -87,17 +101,33 @@ func _on_AnimacaoTransicao_animation_finished(anim_name):
 func _on_AnimationPlayerTxtBox_animation_finished(anim_name):
 	$Botoes/AnimationPlayerBotoes.play("surgindoBotoes")
 
-
 func _on_TouchDia_pressed():
+	ovo = "A"
 	$Botoes/TouchDia.visible = not $Botoes/TouchDia.visible
 	$Botoes/TouchNoite.visible = not $Botoes/TouchNoite.visible
+	_on_btn_pressed()
+	_on_btn_pressed()
 	changeState(State.FINISHED)
 	$"Ovos/OVO-BLUE".visible = not $"Ovos/OVO-BLUE".visible
-	$"Ovos/OVO-BLUE/AnimationOvoAzul".play("mexendo")
+	$"Ovos/OVO-BLUE/AnimationOvoAzul".play("Aparecendo")
 
 func _on_TouchNoite_pressed():
+	ovo = "B"
 	$Botoes/TouchDia.visible = not $Botoes/TouchDia.visible
 	$Botoes/TouchNoite.visible = not $Botoes/TouchNoite.visible
+	_on_btn_pressed()
+	_on_btn_pressed()
 	changeState(State.FINISHED)
 	$"Ovos/OVO-ROSA".visible = not $"Ovos/OVO-ROSA".visible
-	$"Ovos/OVO-ROSA/AnimationOvoRosa".play("mexendo")
+	$"Ovos/OVO-ROSA/AnimationOvoRosa".play("Aparecendo")
+
+func _on_btn_pressed():
+	bt += 1
+
+func _on_AnimationOvoAzul_animation_finished(anim_name):
+	if anim_name == "Aparecendo":
+		$"Ovos/OVO-BLUE/AnimationOvoAzul".play("mexendo")
+
+func _on_AnimationOvoRosa_animation_finished(anim_name):
+	if anim_name == "Aparecendo":
+		$"Ovos/OVO-ROSA/AnimationOvoRosa".play("mexendo")
