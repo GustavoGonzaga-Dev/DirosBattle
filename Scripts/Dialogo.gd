08a56cf01900e3a33af76f8d5bbfda068e25779a
Diego Koga <60438897+DK-diegokoga@users.noreply.github.com>
2021-08-04 22:40:02 -0300

    Mudando o ovo-rosa e colocando os caminhos em variáveis

--- Scripts/Dialogo.gd
+++ Scripts/Dialogo.gd
@@ -5,6 +5,20 @@ onready var textboxContainer = $TextBox/TextBoxContainer
 onready var startS = $TextBox/TextBoxContainer/MarginContainer/HBoxContainer/Start
 onready var endS = $TextBox/TextBoxContainer/MarginContainer/HBoxContainer/End
 onready var lblText = $TextBox/TextBoxContainer/MarginContainer/HBoxContainer/Label
+onready var Ovo_Verde_Animation = $"Ovos/OVO-VERDE/AnimationOvoVerde"
+onready var Ovo_Verde = $"Ovos/OVO-VERDE"
+onready var Ovo_Azul_Animation = $"Ovos/OVO-BLUE/AnimationOvoAzul"
+onready var Ovo_Azul = $"Ovos/OVO-BLUE"
+onready var textboxTween = $TextBox/Tween
+onready var textboxBtn = $TextBox/btn
+onready var textboxProximo = $TextBox/Proximo
+onready var textboxAnimationProximo = $TextBox/Proximo/AnimeProximo
+onready var textboxAnimationPlayer = $TextBox/TextBoxContainer/AnimationPlayerTxtBox
+onready var colorReacAnimacaoTransicao = $"transição/ColorRect/AnimacaoTransicao"
+onready var animationPlayerPlaneta = $planeta/planeta1/AnimationPlayerPlaneta
+onready var animationPlayerBotoes = $Botoes/AnimationPlayerBotoes
+onready var TouchDia = $Botoes/TouchDia
+onready var TouchNoite = $Botoes/TouchNoite
 
 enum State{
 	READY,
@@ -40,7 +54,7 @@ func _process(delta):
 		State.READING:
 			if bt == 2:
 				lblText.percent_visible = 1.0
-				$TextBox/Tween.stop_all()
+				textboxTween.stop_all()
 				endS.text = ">"
 				changeState(State.FINISHED)
 		State.FINISHED:
@@ -64,25 +78,25 @@ func show_TextBox():
 func Display_text():
 	var nextText = textQueue.pop_front()
 	if nextText == "O que você Prefere?":
-		$TextBox/btn.visible = not $TextBox/btn.visible
-		$TextBox/Proximo.visible = not $TextBox/Proximo.visible
-		$TextBox/TextBoxContainer/AnimationPlayerTxtBox.play("subindo")
+		textboxBtn.visible = not textboxBtn.visible
+		textboxProximo.visible = not textboxProximo.visible
+		textboxAnimationPlayer.play("subindo")
 	if nextText == "Muito bom, aqui está o seu OVODIRO...":
-		$TextBox/Proximo.visible = not $TextBox/Proximo.visible
-		$TextBox/btn.visible = not $TextBox/btn.visible
+		textboxProximo.visible = not textboxProximo.visible
+		textboxBtn.visible = not textboxBtn.visible
 	if nextText == "CUIDE MUITO BEM DELE.":
-		$TextBox/Proximo/AnimeProximo.play("Sumindo")
+		textboxAnimationProximo.play("Sumindo")
 		if ovo == "A":
-			$"Ovos/OVO-BLUE/AnimationOvoAzul".play("Sumindo")
+			Ovo_Azul_Animation.play("Sumindo")
 		elif ovo == "B":
-			$"Ovos/OVO-ROSA/AnimationOvoRosa".play("Sumindo")
-		$"transição/ColorRect/AnimacaoTransicao".play("Saindo")
+			Ovo_Verde_Animation.play("Sumindo")
+		colorReacAnimacaoTransicao.play("Saindo")
 	lblText.text = nextText
 	lblText.percent_visible = 0.0
 	changeState(State.READING)
 	show_TextBox()
-	$TextBox/Tween.interpolate_property(lblText, "percent_visible", 0.0, 1.0, len(nextText) * velocidadeLeitura, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	$TextBox/Tween.start()
+	textboxTween.interpolate_property(lblText, "percent_visible", 0.0, 1.0, len(nextText) * velocidadeLeitura, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	textboxTween.start()
 
 func _on_Tween_tween_all_completed():
 	endS.text = ">"
@@ -94,40 +108,40 @@ func changeState(nextState):
 
 func _on_AnimacaoTransicao_animation_finished(anim_name):
 	if anim_name == "Entrando":
-		$planeta/planeta1/AnimationPlayerPlaneta.play("RodaRodaJequiti")
+		animationPlayerPlaneta.play("RodaRodaJequiti")
 	elif anim_name == "Saindo":
 		get_tree().change_scene("res://Cenas/HomeDiro.tscn")
 
 func _on_AnimationPlayerTxtBox_animation_finished(anim_name):
-	$Botoes/AnimationPlayerBotoes.play("surgindoBotoes")
+	animationPlayerBotoes.play("surgindoBotoes")
 
 func _on_TouchDia_pressed():
 	ovo = "A"
-	$Botoes/TouchDia.visible = not $Botoes/TouchDia.visible
-	$Botoes/TouchNoite.visible = not $Botoes/TouchNoite.visible
+	TouchDia.visible = not TouchDia.visible
+	TouchNoite.visible = not TouchNoite.visible
 	_on_btn_pressed()
 	_on_btn_pressed()
 	changeState(State.FINISHED)
-	$"Ovos/OVO-BLUE".visible = not $"Ovos/OVO-BLUE".visible
-	$"Ovos/OVO-BLUE/AnimationOvoAzul".play("Aparecendo")
+	Ovo_Azul.visible = not Ovo_Azul.visible
+	Ovo_Azul_Animation.play("Aparecendo")
 
 func _on_TouchNoite_pressed():
 	ovo = "B"
-	$Botoes/TouchDia.visible = not $Botoes/TouchDia.visible
-	$Botoes/TouchNoite.visible = not $Botoes/TouchNoite.visible
+	TouchDia.visible = not TouchDia.visible
+	TouchNoite.visible = not TouchNoite.visible
 	_on_btn_pressed()
 	_on_btn_pressed()
 	changeState(State.FINISHED)
-	$"Ovos/OVO-ROSA".visible = not $"Ovos/OVO-ROSA".visible
-	$"Ovos/OVO-ROSA/AnimationOvoRosa".play("Aparecendo")
+	Ovo_Verde.visible = not Ovo_Verde.visible
+	Ovo_Verde_Animation.play("Aparecendo")
 
 func _on_btn_pressed():
 	bt += 1
 
 func _on_AnimationOvoAzul_animation_finished(anim_name):
 	if anim_name == "Aparecendo":
-		$"Ovos/OVO-BLUE/AnimationOvoAzul".play("mexendo")
+		Ovo_Azul_Animation.play("mexendo")
 
 func _on_AnimationOvoRosa_animation_finished(anim_name):
 	if anim_name == "Aparecendo":
-		$"Ovos/OVO-ROSA/AnimationOvoRosa".play("mexendo")
+		Ovo_Verde_Animation.play("mexendo")
